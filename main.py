1387066a8c696a1f9ca5a272958801dfca197de4
Liam Z. Charles <liamzcharles@gmail.com>
2019-10-10 16:38:09 +0530

    Sync - 10th Oct 2019

--- main.py
+++ main.py
@@ -1,91 +1,41 @@
-import io
-import picamera
-import logging
-import socketserver
-from threading import Condition
-from http import server
+###############################################################################
+#                                                                             #
+#     A project by:                                                           #
+#                     -----------------------------------                     #
+#                         L I A M  Z.  C H A R L E S                          #
+#                     -----------------------------------                     #
+#                                                                             #
+#                                                                             #
+#                              github.com/1zc                                 #
+###############################################################################
 
-PAGE="""\
-<html>
-    <head>
-        <title>BOBCAM Dashboard</title>
-    </head>
+# DEVELOPED FOR PY3 COMPATIBILITY ONLY.
 
-    <body>
-        <center><h1>>Welcome to BOBCAM Dashboard</h1></center>
-        <center><img src="stream.mjpg" width="640" height="480"></center>
-    </body>
-</html>
-"""
+import time
+import RPi.GPIO as GPIO
+import os
+import subprocess
+import requests
+import configparser
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
+os.system(clear)
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
+print("Checking config files.")
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+initcfg = open('cfg/init.cfg', 'r+')
 
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
+if initcfg.read(1) == '0':
+    print("First time starting. Need to initialize.")
+    # TO-DO: Start HTML setup user-interface.
+    initcfg.close()
+    initcfg = open('cfg/init.cfg', 'w')
+    initcfg.write('1')
+    initcfg.close()
 
-with picamera.PiCamera(resolution='640x480', framerate=60) as camera:
-    output = StreamingOutput()
+print("Loading resources.")
+import stream
 
-    camera.rotation = 180
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 1245)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+
+print("Starting BobCAM.")
+stream.startstream('640x480', 60)
+
