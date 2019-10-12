ee3622a10449f76a47fbc7d7885e28261c796158
Liam Z. Charles <liamzcharles@gmail.com>
2019-10-12 22:24:27 +0530

    Sync - 12th October 2019

--- main.py
+++ main.py
@@ -16,33 +16,63 @@ import RPi.GPIO as GPIO
 import os
 import subprocess
 import requests
+import threading
 import configparser
 
-os.system(clear)
+os.system("clear")
 
 print("Loading config files.")
 
 initcfg = open('cfg/init.cfg', 'r+')
+syscfg = 'cfg/sys.ini'
+config = configparser.ConfigParser()
 
 if initcfg.read(1) == '0':
-    print("First time starting. Need to initialize.")
-    # TO-DO: Start HTML setup user-interface.
     initcfg.close()
+    print("First time starting. Configuration will commence in the CMDline in a few seconds. The web-interface is currently unavailable. ")
+    # TO-DO: Start HTML setup user-interface.
+    #raise SystemExit(0)
+
+    # For now, use cmdline for config interface.
+    print("\n\n\n#####################################\n")
+    print("Welcome to the bobCAM First-Time Configuration!\n\nYou will be asked a few questions to set up your new bobCAM device.\n")
+    print("#####################################\n\n")
+    cfgsysname = input("How would you like the name your bobCAM? \n(Example: Liam's bobCAM, Living Room Cam)\n\n>>>")
+    cfglocmemo = input("\nEnter a brief memo on the location for this bobCAM, useful when you're setting up a network of them. *Leave blank if you'd like to skip this* \n(Example: bobCAM watching the living room from the shelf)\n\n>>>")
+    print("\n\nGreat, we've just completed your bobCAM's first-time configuration! Please wait while we save your settings.")
+    config['sys'] = {}
+    config['sys']['sysname'] = cfgsysname
+    config['sys']['locmemo'] = cfglocmemo
+    config['sys']['strmport'] = 1245            # Video stream port, can be asked but probably shouldn't be.
+    # Ask for video settings - todo later (Could be selected from presets)
+    config['vid'] = {}
+    config['vid']['res'] = "1024x768"
+    config['vid']['fps'] = 45
+    # Default parameters
+    config['DEFAULT'] = {}
+    config['DEFAULT']['strmport'] = 8000
+    with open(syscfg, 'w') as configfile:
+        config.write(configfile)
+
     initcfg = open('cfg/init.cfg', 'w')
     initcfg.write('1')
     initcfg.close()
 
-syscfg = 'cfg/sys.ini'
-config = configparser.ConfigParser()
-config.read(syscfg)
-res = config['vid']['res']
-fps = config['vid']['fps']
+initcfg.close()
 
+config.read(syscfg)
+cfgres = config['vid']['res']
+rawfps = config['vid']['fps']
+rawstrmport = config['sys']['strmport']
+cfgfps = int(rawfps)
+cfgstrmport = int(rawstrmport)
 
 print("Loading resources.")
 import stream
 
 
-print("Starting BobCAM.")
-stream.startstream(res, fps)
+print("Starting BobCAM.\n\n")
+print("Using res="+cfgres+" | fps="+rawfps+" ")
+print("Streaming on network port: "+rawstrmport+"\n\n")
+stream.startstream(cfgres, cfgfps)
 
